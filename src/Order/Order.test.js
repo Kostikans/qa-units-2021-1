489725e5d856c872b31e0cae24727feff2d99ed6
Kostikans <48389219+Kostikans@users.noreply.github.com>
2021-03-11 20:14:43 +0300

    tests

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -1,8 +1,51 @@
 import React from 'react'
+import Order from './Order';
+import { getDate } from '../utils/getDate';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+import {fakeOrders} from "../data/fakeOrders";
+
+jest.mock("../utils/getDate")
+configure({ adapter: new Adapter() });
 
 describe('Order.js', () => {
-  it('some test', () => {
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  })
+
+
+  it('render with no items', () => {
+    const wrapper = shallow(<Order/>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('default render', () => {
+    const wrapper = shallow(<Order order={fakeOrders[0]}/>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('getDate call', () => {
+    const wrapper = shallow(<Order order={fakeOrders[0]}/>);
+    expect(getDate).toHaveBeenCalled()
+  });
+
+
+  it('no date and shops', () => {
+    const order = {}
+    const wrapper = shallow(<Order order={order}/>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+
+  it('no items in order', () => {
+    const order = {
+      date: 243,
+      shop: 'shop'
+    }
+    const wrapper = shallow(<Order order={order}/>);
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
 
--- src/utils/sortOrders.test.js
+++ src/utils/sortOrders.test.js
@@ -1,11 +1,32 @@
 import React from 'react'
-import {sortByItemCount} from './sortOrders';
+import { unstable_batchedUpdates } from 'react-dom';
+import { fakeOrders } from '../data/fakeOrders';
+import {getSortFunction, sortByDate, sortByItemCount, sortOrders, sortTypes} from './sortOrders';
 
 describe('sortByItemCount function', () => {
 	it('orders are null', () => {
 		const result = sortByItemCount(null, null);
 		expect(result).toEqual(0);
 	});
+
+	it('orders are not object', () => {
+		const result = sortByItemCount(Int16Array, Int16Array);
+		expect(result).toEqual(0);
+	});
+
+	it('items null', () => {
+		const order1 = {
+			items: null,
+		};
+
+		const order2 = {
+			items:  null,
+		};
+
+		const result = sortByItemCount(order1, order2);
+		expect(result).toEqual(0);
+	});
+
 
 	it('same items count', () => {
 		const order1 = {
@@ -22,3 +43,93 @@ describe('sortByItemCount function', () => {
 	});
 });
 
+describe('sortOrders function', () => {
+	it('orders are null', () => {
+		const result = sortOrders(null,getSortFunction(sortTypes.COUNT));
+		expect(result).toEqual(undefined);
+	});
+
+	it('sortfunction are null', () => {
+
+		const order1 = {
+			items: ['item1', 'item2'],
+		};
+
+		const order2 = {
+			items: ['1', '2'],
+		};
+
+		const orders = [order1,order2]
+
+		const result = sortOrders(orders, null);
+		expect(result).toEqual(undefined);
+	});
+});
+
+
+describe('sortByDate function', () => {
+	it('orders are null', () => {
+		const result = sortByDate(null, null);
+		expect(result).toEqual(0);
+	});
+
+	it('orders are not object', () => {
+		const result = sortByDate(Int16Array, Int16Array);
+		expect(result).toEqual(0);
+	});
+
+	it('not date', () => {
+		const order1 = {
+			items: ['item1', 'item2'],
+		};
+
+		const order2 = {
+			items: ['1', '2'],
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+	it('date1 < date2', () => {
+		const order1 = {
+			date: 154234232432,
+		};
+
+		const order2 = {
+			date: 15523424,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(-1);
+	});
+
+	it('date1 > date2', () => {
+		const order1 = {
+			date: 15523424,
+		};
+
+		const order2 = {
+			date: 154234232432,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(1);
+	});
+
+	it('date1 = date2', () => {
+		const order1 = {
+			date: 154234232432,
+		};
+
+		const order2 = {
+			date: 154234232432,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+});
