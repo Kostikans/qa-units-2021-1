79c093113efb495e0ce0a5ffa8c8d0dae5006294
Kostikans <48389219+Kostikans@users.noreply.github.com>
2021-03-11 22:01:20 +0300

    fix

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Order from './Order';
-import { getDate } from '../utils/getDate';
-import {shallow, configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import {fakeOrders} from "../data/fakeOrders";
-
 jest.mock("../utils/getDate")
+import { getDate } from '../utils/getDate';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { fakeOrders } from "../data/fakeOrders";
+
 configure({ adapter: new Adapter() });
 const fakeDate = 1234;
 describe('Order.js', () => {
@@ -17,24 +17,24 @@ describe('Order.js', () => {
 
 
   it('render with no items', () => {
-    const wrapper = shallow(<Order/>);
+    const wrapper = shallow(<Order />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('render with data', () => {
-    const wrapper = shallow(<Order order={{shop: "shop", date: 1234}}/>);
-    expect((wrapper)).toMatchSnapshot();
+    const wrapper = shallow(<Order order={{ shop: "shop", date: 1234 }} />);
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('getDate call', () => {
-    const wrapper = shallow(<Order order={fakeOrders[0]}/>);
+    const wrapper = shallow(<Order order={fakeOrders[0]} />);
     expect(getDate).toHaveBeenCalled()
   });
 
 
   it('no date and shops', () => {
     const order = {}
-    const wrapper = shallow(<Order order={order}/>);
+    const wrapper = shallow(<Order order={order} />);
     expect(wrapper).toMatchSnapshot();
   });
 
@@ -44,7 +44,7 @@ describe('Order.js', () => {
       date: 1234,
       shop: 'shop'
     }
-    const wrapper = shallow(<Order order={order}/>);
+    const wrapper = shallow(<Order order={order} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
--- src/utils/sortOrders.test.js
+++ src/utils/sortOrders.test.js
@@ -1,32 +1,31 @@
 import React from 'react'
 import { unstable_batchedUpdates } from 'react-dom';
 import { fakeOrders } from '../data/fakeOrders';
-import {getSortFunction, sortByDate, sortByItemCount, sortOrders, sortTypes} from './sortOrders';
+import { getSortFunction, sortByDate, sortByItemCount, sortOrders, sortTypes } from './sortOrders';
 
 describe('sortByItemCount function', () => {
 	const cases = [
-		[{items: ['item1', 'item2']}, {items: ['1', '2']}, 0],
-		[{items: ['item1']}, {items: ['1', '2']}, -1],
-		[{items: ['item1', 'item2']}, {items: ['1']}, 1],
+		[{ items: ['item1', 'item2'] }, { items: ['1', '2'] }, 0],
+		[{ items: ['item1'] }, { items: ['1', '2'] }, -1],
+		[{ items: ['item1', 'item2'] }, { items: ['1'] }, 1],
 		[Int16Array, Int16Array, 0],
-		[{items: null}, {items: null}, 0],
-		[null,null,0]
+		[{ items: null }, { items: null }, 0],
+		[null, null, 0]
 	];
 
-	test.each(cases)('(%i, %i , %i)',(firstArg, secondArg, expectedResult) => {
-        const result = sortByItemCount(firstArg, secondArg);
-        expect(result).toBe(expectedResult);
-    })
+	test.each(cases)('(%i, %i , %i)', (firstArg, secondArg, expectedResult) => {
+		const result = sortByItemCount(firstArg, secondArg);
+		expect(result).toBe(expectedResult);
+	})
 });
 
 describe('sortOrders function', () => {
 	it('orders are null', () => {
-		const result = sortOrders(null,getSortFunction(sortTypes.COUNT));
+		const result = sortOrders(null, getSortFunction(sortTypes.COUNT));
 		expect(result).toEqual(undefined);
 	});
 
 	it('sortfunction are null', () => {
-
 		const order1 = {
 			items: ['item1', 'item2'],
 		};
@@ -35,7 +34,7 @@ describe('sortOrders function', () => {
 			items: ['1', '2'],
 		};
 
-		const orders = [order1,order2]
+		const orders = [order1, order2]
 
 		const result = sortOrders(orders, null);
 		expect(result).toEqual(undefined);
@@ -55,16 +54,16 @@ describe('sortByDate function', () => {
 		expect(result).toEqual(0);
 	});
 	const cases = [
-        [{items: ['item1', 'item2']}, {items: ['1', '2']}, 0],
-        [{date: 154234232432}, {date: 15523424}, -1],
-        [{date: 15523424}, {date: 154234232432}, 1],
-        [ {date:154234232432},{date:154234232432}, 0],
+		[{ items: ['item1', 'item2'] }, { items: ['1', '2'] }, 0],
+		[{ date: 154234232432 }, { date: 15523424 }, -1],
+		[{ date: 15523424 }, { date: 154234232432 }, 1],
+		[{ date: 154234232432 }, { date: 154234232432 }, 0],
 		[Int16Array, Int16Array, 0],
-		[null,null,0]
+		[null, null, 0]
 	];
-	
-	test.each(cases)('(%i, %i , %i)',(firstArg, secondArg, expectedResult) => {
-        const result = sortByDate(firstArg, secondArg);
-        expect(result).toBe(expectedResult);
-    })
+
+	test.each(cases)('(%i, %i , %i)', (firstArg, secondArg, expectedResult) => {
+		const result = sortByDate(firstArg, secondArg);
+		expect(result).toBe(expectedResult);
+	})
 });
